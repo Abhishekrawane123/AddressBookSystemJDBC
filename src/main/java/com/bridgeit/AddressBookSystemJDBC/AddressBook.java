e9011ede9a685798e7cda152c8c6bfbf1c3ad53d
pknowledge <abhishekrawane1195@gmail.com>
2021-12-29 21:43:04 +0530

    Added : Refactored the program to add Person Details

--- src/main/java/com/bridgeit/AddressBookSystemJDBC/AddressBook.java
+++ src/main/java/com/bridgeit/AddressBookSystemJDBC/AddressBook.java
@@ -1,28 +1,89 @@
 package com.bridgeit.AddressBookSystemJDBC;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
-	private final String FirstName, LastName, Address, CityName, StateName;
-	public final double zip;
-	public final double PhoneNumber;
-	private final String EmailID;
+	private final String firstName;
+	private final String lastName;
+	private final String address;
+	private final String cityName;
+	private final String stateName;
+	private final String zip;
+	private String phoneNumber;
 
-	public AddressBook(String FirstName, String LastName, String Address, String CityName, String StateName, double zip,
-			double PhoneNumber, String EmailID) {
+	public AddressBook(String firstName, String lastName, String address, String cityName, String stateName, String zip,
+			String phoneNumber) {
 		super();
-		this.FirstName = FirstName;
-		this.LastName = LastName;
-		this.Address = Address;
-		this.CityName = CityName;
-		this.StateName = StateName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.cityName = cityName;
+		this.stateName = stateName;
 		this.zip = zip;
-		this.PhoneNumber = PhoneNumber;
-		this.EmailID = EmailID;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	@Override
+	public String toString() {
+		return "AddressBook [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", cityName="
+				+ cityName + ", stateName=" + stateName + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
+	}
+
+	public static AddressBook contactPerson() {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("first name : ");
+		String firstName = scanner.next();
+		System.out.println("last name : ");
+		String lastName = scanner.next();
+		System.out.println("address : ");
+		String address = scanner.next();
+		System.out.println("city name : ");
+		String cityName = scanner.next();
+		System.out.println("stste name : ");
+		String stateName = scanner.next();
+		System.out.println("zip : ");
+		String zip = scanner.next();
+		System.out.println("phone number : ");
+		String phoneNumber = scanner.next();
+		AddressBook addressBook = new AddressBook(firstName, lastName, address, cityName, stateName, zip,
+				phoneNumber);
+		return addressBook;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Wel-Come to Address_Book_System Program");
 		
+		System.out.println("welcome to the address book program");
+		AddressBook addressBook =contactPerson();
+		System.out.println(addressBook);		
 	}
-	
 }
